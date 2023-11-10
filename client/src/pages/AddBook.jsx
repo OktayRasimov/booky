2715812acfb65f5ba3a2d9c71a25fcbb859fddb3
Oktay Rasimov <oktayrasimov123@gmail.com>
2023-11-10 16:55:14 +0200

    made addBook functional and did some form fixes

--- client/src/pages/AddBook.jsx
+++ client/src/pages/AddBook.jsx
@@ -2,16 +2,33 @@ import axios from "axios";
 import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import BackButton from "../components/BackButton";
 
-const StyledAddBookContainer = styled.div`
-  background-color: red;
+const StyledAddBookContainer = styled.form`
+  background-color: var(--color-white-100);
   /* width: fit-content; */
-  padding: 1rem;
+  padding: 1rem 2rem;
+  border-radius: 10px;
   display: flex;
   flex-direction: column;
+  width: 50rem;
   transform: translateY(-20%);
-  button {
-    height: 50px;
+  section {
+    p {
+      font-size: 2.4rem;
+      font-weight: 600;
+    }
+    input {
+      width: 100%;
+      padding: 0.6rem 0.4rem;
+      border-radius: 5px;
+      border: 1px solid;
+    }
+  }
+  aside {
+    margin-top: 2rem;
+    display: flex;
+    justify-content: space-between;
   }
 `;
 
@@ -37,17 +54,17 @@ function AddBook() {
       .catch((err) => {
         console.log(err.message);
       })
-      .finally(navigate(-1));
+      .finally(() => navigate(-1));
   }
 
   return (
-    <StyledAddBookContainer>
+    <StyledAddBookContainer onSubmit={handleAddBook}>
       <section>
         <h1>Add a book</h1>
         <p>Title</p>
-        <input ref={title} placeholder="Book Title..." />
+        <input ref={title} placeholder="Book Title..." required />
         <p>Author</p>
-        <input ref={author} placeholder="Author..." />
+        <input ref={author} placeholder="Author..." required />
         <p>Description</p>
         <input ref={description} placeholder="Description..." />
         <p>Completed</p>
@@ -56,7 +73,10 @@ function AddBook() {
           <option value="true">true</option>
         </select>
       </section>
-      <button onClick={handleAddBook}>ADD</button>
+      <aside>
+        <BackButton />
+        <button type="submit">ADD</button>
+      </aside>
     </StyledAddBookContainer>
   );
 }
