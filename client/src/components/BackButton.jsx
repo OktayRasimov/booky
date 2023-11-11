5bd42e96b7ac34585dc71dc9103095ac9613ee64
Oktay Rasimov <oktayrasimov123@gmail.com>
2023-11-11 16:15:35 +0200

    fixes to add book

--- client/src/components/BackButton.jsx
+++ client/src/components/BackButton.jsx
@@ -2,13 +2,13 @@ import { useNavigate } from "react-router-dom";
 import { HiChevronDoubleLeft } from "react-icons/hi";
 import styled from "styled-components";
 
-const StyledBackButton = styled.p`
+const StyledBackButton = styled.div`
   font-size: 1.8rem;
   /* background-color: red; */
   width: fit-content;
   display: flex;
   align-items: center;
-  gap: 0.8rem;
+  gap: 0.4rem;
 `;
 
 function BackButton() {
@@ -21,7 +21,7 @@ function BackButton() {
       }}
     >
       <HiChevronDoubleLeft />
-      <h3>Previous</h3>
+      <h4>Previous</h4>
     </StyledBackButton>
   );
 }
