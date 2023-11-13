a96016ab84119b4b123b7a2f998f4a3eeb598fda
Oktay Rasimov <oktayrasimov123@gmail.com>
2023-11-13 19:23:32 +0200

    fixed mongodb url typo

--- server/config.js
+++ server/config.js
@@ -1,3 +1,3 @@
 export const PORT = process.env.PORT || 8888;
 export const url =
-  "mongodb+srv://oktayrasimov:AL63EmLeLux7Gbsf@cluster0.hnkolwk.mongodb.net/book-store-api?retryWrites=true&w=majority";
+  "mongodb+srv://oktayrasimov:AL63EmLeLux7Gbsf@cluster0.hnkolwk.mongodb.net/?retryWrites=true&w=majority";
